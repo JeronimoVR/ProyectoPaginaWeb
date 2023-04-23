0012f0acc78525f08524937116e25aaa5ca69a40
Jeronimo Velez Rojas <velezrojasjeronimo@gmail.com>
2023-04-23 10:38:50 -0500

    mejorando

--- app/Views/template/cabecera.php
+++ app/Views/template/cabecera.php
@@ -12,6 +12,10 @@
       background: linear-gradient(90deg, #1c3643, #273b47 25%, #1e5372);
     }
 
+    nav {
+      background-color: #b2d9f7;
+    }
+
     #footer a {
       color: white;
     }
@@ -20,11 +24,11 @@
 
 <body>
 
-  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <nav class="navbar navbar-expand-lg navbar-light ">
 
     <div class="container">
 
-      <a class="navbar-brand" href="#">Tu cocina</a>
+      <a class="navbar-brand fs-3 text" href="#">Tu cocina</a>
 
       <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
@@ -43,7 +47,7 @@
           </li>
 
           <li class="nav-item">
-            <a class="nav-link active">Ensaladas</a>
+            <a class="nav-link active" href="#">Ensaladas</a>
           </li>
 
           <li class="nav-item">
@@ -51,26 +55,25 @@
           </li>
 
           <li class="nav-item dropdown">
-            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-              Autores
+            <a class="nav-link dropdown-toggle fst-italic" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+              Usuario <strong><?php echo session('usuario') ?></strong>
             </a>
             <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-              <li><a class="dropdown-item" href="#">Jeronimo</a></li>
-              <li><a class="dropdown-item" href="#">Taylor</a></li>
-              <li><a class="dropdown-item" href="#">Nicole</a></li>
+              <li><a class="dropdown-item" href="#">Cuenta <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-person-circle" viewBox="0 0 16 16">
+                    <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0z" />
+                    <path fill-rule="evenodd" d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8zm8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1z" />
+                  </svg></a></li>
+              <li><a class="dropdown-item" href="<?= base_url('/salir'); ?>">Cerrar sesión <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-box-arrow-in-right" viewBox="0 0 16 16">
+                    <path fill-rule="evenodd" d="M6 3.5a.5.5 0 0 1 .5-.5h8a.5.5 0 0 1 .5.5v9a.5.5 0 0 1-.5.5h-8a.5.5 0 0 1-.5-.5v-2a.5.5 0 0 0-1 0v2A1.5 1.5 0 0 0 6.5 14h8a1.5 1.5 0 0 0 1.5-1.5v-9A1.5 1.5 0 0 0 14.5 2h-8A1.5 1.5 0 0 0 5 3.5v2a.5.5 0 0 0 1 0v-2z" />
+                    <path fill-rule="evenodd" d="M11.854 8.354a.5.5 0 0 0 0-.708l-3-3a.5.5 0 1 0-.708.708L10.293 7.5H1.5a.5.5 0 0 0 0 1h8.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3z" />
+                  </svg></a></li>
             </ul>
           </li>
 
-          <li class="nav-item dropdown">
-            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            Usuario <?php echo session('usuario')?>
-            </a>
-            <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-              <li><a class="dropdown-item" href="#">Cuenta</a></li>
-              <li><a class="dropdown-item" href="<?= base_url('/salir'); ?>">Cerrar sesión</a></li>
-            </ul>
-          </li>
         </ul>
+
       </div>
+
     </div>
+
   </nav>
